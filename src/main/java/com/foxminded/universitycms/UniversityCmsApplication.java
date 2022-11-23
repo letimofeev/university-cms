d2d22d3d2e5a51a2a985c1fb9224e3fd88587a1b
leonid <kotovpetia@gmail.com>
2022-11-23 10:57:52 +0300

    Spring boot app initialize

---
+++ src/main/java/com/foxminded/universitycms/UniversityCmsApplication.java
@@ -0,0 +1,12 @@
+package com.foxminded.universitycms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityCmsApplication.class, args);
+    }
+}
