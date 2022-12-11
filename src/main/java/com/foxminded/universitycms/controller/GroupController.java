8070779250d639834c8366dad4f4eac1b6b46ca1
leonid <kotovpetia@gmail.com>
2022-12-11 23:56:35 +0300

    GroupController added

---
+++ src/main/java/com/foxminded/universitycms/controller/GroupController.java
@@ -0,0 +1,26 @@
+package com.foxminded.universitycms.controller;
+
+import com.foxminded.universitycms.model.Group;
+import com.foxminded.universitycms.service.GroupService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class GroupController {
+
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+    @GetMapping("/show-all-groups")
+    public String showAllGroups(Model model) {
+        List<Group> groups = groupService.findAll();
+        model.addAttribute("allGroups", groups);
+        return "all-groups";
+    }
+}
