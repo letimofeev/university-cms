a74e3859bbd53cc644939cb9dee5c340c46609ee
leonid <kotovpetia@gmail.com>
2022-12-12 19:58:27 +0300

    StudentControllerTest.java added

---
+++ src/test/java/com/foxminded/universitycms/controller/StudentControllerTest.java
@@ -0,0 +1,43 @@
+package com.foxminded.universitycms.controller;
+
+import com.foxminded.universitycms.model.Student;
+import com.foxminded.universitycms.service.StudentService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(StudentController.class)
+class StudentControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private StudentService studentService;
+
+    @Test
+    void showAllLessons_shouldContainExpectedModel_whenGetRequest() throws Exception {
+        List<Student> students = List.of(new Student(9, new ArrayList<>()));
+
+        given(studentService.findAll()).willReturn(students);
+
+        mvc.perform(get("/show-all-students"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("students"))
+                .andExpect(model().attributeExists("students"))
+                .andExpect(model().attribute("students", students));
+    }
+
+}
